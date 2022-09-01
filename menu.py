b61ff17e4f1207cf81f0479c628fcd55b1ce8887
navithsanjay <112268197+navithsanjay@users.noreply.github.com>
2022-09-01 19:15:40 +0530

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,32 @@
+import tkinter as tki
+a=tki.TK()
+menubar=tki.Menu(a)
+file=tki.Menu(menubar,tearoff=0)
+menubar.add_cascade(label='File',menu=file)
+file.add_command(label='New File',command=None)
+file.add_command(label='Open',command=None)
+file.add_command(label='Save',command=None)
+file.add_separator()
+file.add_command(label='Exit',command=None)
+
+
+edit=tki.Menu(menubar,tearoff=0)
+menubar.add_cascade(label='Edit',menu=edit)
+edit.add_command(label='Edit',command=None)
+edit.add_command(label='Cut',command=None)
+edit.add_command(label='Copy',command=None)
+edit.add_command(label='Paste',command=None)
+edit.add_command(label='Select all',command=None)
+edit.add_separator()
+edit.add_command(label='Find...',command=None)
+edit.add_command(label='Find again',command=None)
+
+help=tki.Menu(menubar,tearoff=0)
+menubar.add_cascade(label='Help',menu=help)
+help.add_command(label='Help',command=None)
+help.add_command(label='Remainder Help',command=None)
+help.add_command(label='Demo',command=None)
+help.add_separator()
+help.add_command(label='About Remaider',command=None)
+
+a.configure(menu=menubar)
